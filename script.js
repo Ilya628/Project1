385cddbe0ca5e9c2a67e825046f667ee0ad677c4
Ilya <tolstobrov.ilia@gmail.com>
2024-11-05 00:04:14 +0300

    first steps

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 "use strict";
-
-alert('Hi');
-console.log("Hello suka");
+const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
